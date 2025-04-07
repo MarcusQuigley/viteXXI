93e55f1aaf14ede9b25c0de89fc504976ef64c38
marcusquigley <marcusquigley@gmail.com>
2025-04-07 13:39:42 -0700

    adding filtering

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,21 @@ const list = [
 ];
 
 
+const isSearched = (st)=> (item) =>
+  {
+  return item.title.toLowerCase().includes(st.toLowerCase());
+  }
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.onDismiss = this.onDismiss.bind(this)
-
+    this.onSearchChange = this.onSearchChange.bind(this)
     this.state = {
       list,
+      searchTerm: '',
     };
 
   }
@@ -38,14 +44,20 @@ class App extends Component {
   onDismiss(id) {
     const updatedList = this.state.list.filter(item => item.objectID != id)
     this.setState({ list: updatedList })
-    //console.log(updatedList)
   }
-
+ 
+  onSearchChange(event){
+    this.setState({searchTerm: event.target.value})
+  }
 
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input type='text'
+                 onChange={this.onSearchChange} />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
